55f019c8db8162754ab51d102ab28e55b68902fb
saurabhdata <saurabhdata.python@gmail.com>
2022-10-09 16:04:21 +0530

    hi

--- Constants.py
+++ Constants.py
@@ -1,3 +1,3 @@
-GitHubLink = "https://github.com/mdFaizz/iNeuronYoutubeChallenge.git"
+GitHubLink = "https://github.com/saurabhdatapython/tuberscrap"
 
 api_key = "AIzaSyC09Ez4gChOO3tnbGm1uEMuIGOmu6LDj2E"
